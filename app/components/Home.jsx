18d282891253b1beb9fda453ad5770ddac5a7813
Lakshay Bansal <lakshay.bansal@wipro.com>
2016-12-05 20:38:58 +0530

    Pushed the code with things data and map

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+var  React=require('react');
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
+var d=[];
 var Home = React.createClass({
+
+getInitialState:function(){
+   return { metricAll:[] };
+},
+  componentDidMount:function(){
+  var _this=this;
+  this.serverRequest=axios.get('/api/getThings').then(function(data){
+    console.log('Success');
+    console.log(data);
+    _this.setState({metricAll:data.data.things});
+  }, function(err){
+    console.log(err);
+  });
+  },
   render: function(){
-    return (
-      <div>
-        <p>Home Page</p>
-        <p>Metrics</p>
-      </div>
+  return (
+    <div>
+        <h1>Home</h1>
+        {this.state.metrics}
+        <h1>Metrics are</h1>
+          {this.state.metricAll.map(function(metric) {
+            return (
+              <div key={metric.metric_key} >
+                Metric is {metric.name}
+              </div>
     );
+  })}
+  </div>
+  )
   }
-})
+});
 
 module.exports = Home;
